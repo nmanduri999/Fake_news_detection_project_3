137f15a32096f6b852bbf5cdb8e7d95f14df0070
Tom Recht <rechttom@gmail.com>
2020-08-13 20:44:08 -0700

    working website

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+import nltk
+from nltk.stem import WordNetLemmatizer
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from joblib import dump, load
+
+model = load_model("neural_network.h5")
+tfIdfVectorizer=load('tfidfV.joblib')
+
+def findClass(new_data):
+    wl = WordNetLemmatizer()
+    stopwords= nltk.corpus.stopwords.words('english')
+    words = re.split('\W+', new_data)
+    text = []
+    for word in words:
+        if word not in stopwords:
+            text.append(wl.lemmatize(word))
+    new_text = [" ".join(text)]
+    new_tfidf = tfIdfVectorizer.transform(new_text)
+    new_tfidf.sort_indices()
+    new_pred = model.predict_classes(new_tfidf)
+    if new_pred[0] == 0:
+        return "True"
+    else:
+        return "Fake"
 
 
 app = Flask(__name__)
@@ -7,8 +33,11 @@ app = Flask(__name__)
 def home():
 
     article = request.form.get('article')
-
-    return render_template("index.html", article=article)
+    if article:
+        result = findClass(str(article))
+    else:
+        result = None
+    return render_template("index.html", article=article, result=result)
 
 
 if __name__ == "__main__":
